96d03afee5739cab54f45601b523fd4122ce0e50
Abhilasha Bansal <64535297+AbhilashaBansal@users.noreply.github.com>
2020-12-23 21:50:40 +0530

    Create lca.cpp

---
+++ lca.cpp
@@ -0,0 +1,15 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root==p || root==q){
+            return root;
+        }
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+        if(left && right){
+            return root;
+        }
+        if(left) return left;
+        return right;
+}
